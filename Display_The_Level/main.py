2fb0a32f987037028e1630725857c251670bdfe1
molfaryura <oleksandrr1996@protonmail.com>
2025-05-08 14:51:16 +0300

    Update script

--- Display_The_Level/main.py
+++ Display_The_Level/main.py
@@ -7,8 +7,7 @@ pin = range(6, 16)
 led = []
 
 for i in range(10):
-    led.append(None)
-    led[i] = machine.Pin(pin[i], machine.Pin.OUT)
+    led.append(machine.Pin(pin[i], machine.Pin.OUT))
 
 while True:
     for i in range(10):
